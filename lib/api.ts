def78b169a20dca678b9eef63b4efcb40464b0a2
Oskar Puchalski <oskarpuchalski17@gmail.com>
2022-10-15 12:04:19 +0200

    minor improvements and deleted comments

--- lib/api.ts
+++ lib/api.ts
@@ -39,7 +39,6 @@ export function getAllPosts(fields: string[] = []): PostItems[] {
   const slugs = getPostSlugs();
   const posts = slugs
     .map((slug) => getPostBySlug(slug, fields))
-    // sort posts by date in descending order
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
   return posts;
 }
