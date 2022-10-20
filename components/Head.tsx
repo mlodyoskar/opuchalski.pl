de13131d5c327efb7564e9e86687dbb51a180cd8
Oskar Puchalski <oskarpuchalski17@gmail.com>
2022-10-20 19:45:42 +0200

    feat: added favicon

--- components/Head.tsx
+++ components/Head.tsx
@@ -22,6 +22,24 @@ const Head = ({ customMeta }: { customMeta?: MetaProps }) => {
       <meta content={meta.description} name="description" />
       <meta property="og:url" content={`${WEBSITE_HOST_URL}${router.asPath}`} />
       <link rel="canonical" href={`${WEBSITE_HOST_URL}${router.asPath}`} />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="favicons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="favicons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="favicons/favicon-16x16.png"
+      />
+      <link rel="manifest" href="favicons/site.webmanifest" />
       <meta property="og:type" content={meta.type} />
       <meta property="og:site_name" content="Oskar Puchalski • Blog" />
       <meta property="og:description" content={meta.description} />
