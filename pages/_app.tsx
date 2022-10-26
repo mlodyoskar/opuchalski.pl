06bb2b5b923ef16a662d086ae52eb27dfe4dc5a7
Oskar Puchalski <oskarpuchalski17@gmail.com>
2022-10-26 19:57:51 +0200

    feat: added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
-import React from 'react';
 import '../styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
-const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <ThemeProvider attribute="class" enableSystem={true} defaultTheme="dark">
+        <Component {...pageProps} />
+      </ThemeProvider>
+      <Analytics />
+    </>
   );
 };
 
