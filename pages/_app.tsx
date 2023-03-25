06f728785d898b95a7483663627c68dc858b8ddd
Oskar Puchalski <oskarpuchalski17@gmail.com>
2023-03-25 14:28:17 +0100

    feat: added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,27 @@ import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.css';
+import { useEffect } from 'react';
+import Router from 'next/router';
+import * as gtag from '../lib/gtag';
+
+const isProduction = process.env.NODE_ENV === 'production';
+const isBrowser = typeof window !== 'undefined';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    if (isProduction && isBrowser) {
+      const handleRouteChange = (url: string) => {
+        gtag.reportPageView(url);
+      };
+      Router.events.on('routeChangeComplete', handleRouteChange);
+      return () => {
+        Router.events.off('routeChangeComplete', handleRouteChange);
+      };
+    }
+    return () => false;
+  }, []);
+
   return (
     <>
       <ThemeProvider attribute="class" enableSystem={true} defaultTheme="dark">
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export const Document = () => {
   return (
@@ -7,6 +8,25 @@ export const Document = () => {
       <body className="bg-bg-light text-gray-900 dark:bg-bg-dark dark:text-white">
         <Main />
         <NextScript />
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+        />
+
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GA_TRACKING_ID}', {
+              page_path: window.location.pathname
+            });
+          `.trim(),
+          }}
+        />
       </body>
     </Html>
   );
