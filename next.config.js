92675e7762af2e14fecb3a545420a375edfb8733
Oskar Puchalski <oskarpuchalski17@gmail.com>
2023-07-06 16:09:29 +0200

    feat: added cv and redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 // * @type {import('next').NextConfig}
 
 module.exports = {
+    async redirects() {
+        return [
+            {
+                source: '/cv',
+                destination: '/CV_Oskar_Puchalski.pdf',
+                permanent: true,
+            },
+        ]
+    },
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/i,
